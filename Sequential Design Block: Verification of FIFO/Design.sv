09b848bb55095ca05884284c5d8ba0aa603fc58a
sarawiRTLDV <109423168+sarawiRTLDV@users.noreply.github.com>
2022-09-15 02:43:08 +0100

    Update Design.sv

--- Sequential Design Block: Verification of FIFO/Design.sv
+++ Sequential Design Block: Verification of FIFO/Design.sv
@@ -1,19 +1,30 @@
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++
+-> here the design of the fifo
+*/
+
 module fifo( 
+  /*
+  -> here we declare our inputs and output ports of the fifo module 
+  -> the clock and rst are global signals 
+  -> remember in our testbench we will be generating the stimulus for rd, wr, rst and data signals using pseudo random generator, but for the clock signal we will be generating it inside our tb top();
+  */
   input clock, rd, wr,
-  output full, empty,
+  output full, empty, // these two signals are for representing the status of our fifo 
   input [7:0] data_in,
   output reg [7:0] data_out,
   input rst);
  
  
-  reg [7:0] mem [31:0];
+  reg [7:0] mem [31:0];// here we basicaly creating a mem storage with the capability to store 32 elemement 8bits each
+  // these two pointers are for mem adressing
   reg [4:0] wr_ptr;
   reg [4:0] rd_ptr; 
  
  
- 
-always@(posedge clock)
+ // now lets understand the mean code of our design
+  always@(posedge clock) // this means that our design is sensitive to the positive edge of the clock
   begin
+    /*->if reset is high we will initialize the fifo to it's initial state*/
     if (rst == 1'b1)
       begin
         data_out <= 0;
@@ -24,17 +35,20 @@ always@(posedge clock)
         end
       end
     else
+      /*if the reset is low*/
       begin
+        /*here we will be cheking if the user wants to write into the fifo or read from it */
+        
         if ((wr == 1'b1)  && (full == 1'b0))
           begin
           mem[wr_ptr] <= data_in;
-          wr_ptr = wr_ptr + 1;
+          wr_ptr = wr_ptr + 1;//to not overwrite a memory data
           end
         
         if((rd == 1'b1) && (empty == 1'b0))
           begin
-          data_out <= mem[rd_ptr];
-          rd_ptr <= rd_ptr + 1;
+            data_out <= mem[rd_ptr];// here the user will be reading the oldest memory data in our fifo
+          rd_ptr <= rd_ptr + 1;// for not reading the same address
           end
       end
 end
@@ -44,8 +58,6 @@ assign empty = ((wr_ptr - rd_ptr) == 0) ? 1'b1 : 1'b0;
   
 assign full = ((wr_ptr - rd_ptr) == 31) ? 1'b1 : 1'b0; 
   
-endmodule
- 
 endmodule
  
  
